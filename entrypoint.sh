f5595f3232a448bfd9e1825ddce96567ce9d5704
Alexander Khryukin <alexander@mezon.ru>
2016-01-25 03:10:28 +0300

    add u-boot build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export KERNEL_VER="$KERNEL_VER"
 
+build_kernel() {
 # go to root dir
 cd
 git clone https://github.com/RobertCNelson/armv7-multiplatform
@@ -9,5 +9,28 @@ cd armv7-multiplatform/
 # git checkout origin/v4.4.x-rt -b tmp etc
 git checkout origin/${KERNEL_VER} -b tmp
 ./build_kernel.sh
+cp -rv /root/armv7-multiplatform/deploy /tmp/
+}
 
-cp -r /root/armv7-multiplatform/deploy /tmp/
+build_uboot() {
+# go to root dir
+cd
+wget -c https://releases.linaro.org/components/toolchain/binaries/5.2-2015.11/arm-linux-gnueabihf/gcc-linaro-5.2-2015.11-x86_64_arm-linux-gnueabihf.tar.xz
+tar xf gcc-linaro-5.2-2015.11-x86_64_arm-linux-gnueabihf.tar.xz
+export CC=`pwd`/gcc-linaro-5.2-2015.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+git clone https://github.com/u-boot/u-boot
+pushd u-boot
+git checkout v2016.01 -b tmp
+wget -c https://rcn-ee.com/repos/git/u-boot-patches/v2016.01/0001-wandboard-uEnv.txt-bootz-n-fixes.patch
+patch -p1 < 0001-wandboard-uEnv.txt-bootz-n-fixes.patch
+make ARCH=arm CROSS_COMPILE=${CC} distclean
+make ARCH=arm CROSS_COMPILE=${CC} wandboard_defconfig
+# threads
+make ARCH=arm CROSS_COMPILE=${CC} -j$(getconf _NPROCESSORS_ONLN)
+# copy built bootloader stuff
+mv -f SPL /tmp/deploy/SPL
+mv -f u-boot.img /tmp/deploy
+}
+
+build_uboot
+build_kernel
